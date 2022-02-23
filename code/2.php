593ca3789a39802a2b86d2940a8b36f1a976f55f
S39G <sega.zhukov.75@mail.ru>
2022-02-24 01:18:25 +0200

    punkt 2

---
+++ code/2.php
@@ -0,0 +1,20 @@
+<?php
+
+$xInt = 4;
+echo $xInt;
+
+$yInt = 8;
+echo "\n<br>"; 
+echo $yInt;
+
+$zFloat = 4.8;
+echo "\n<br>";
+echo $zFloat;
+
+echo "\n<br>";
+echo 13-1;
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo "\n<br>";
+echo $last_month - $this_month;
